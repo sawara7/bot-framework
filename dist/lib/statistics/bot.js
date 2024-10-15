d1fb948c88a7178fbe4cc0812f4a1db9d5e88167
sawara7 <sawara7@gmail.com>
2024-10-15 10:48:12 +0900

    commit

--- dist/lib/statistics/bot.js
+++ dist/lib/statistics/bot.js
@@ -12,6 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.TickerStatisticsCustomeClass = void 0;
 const bot_1 = require("../base/bot");
 const __1 = require("../..");
+const utils_general_1 = require("utils-general");
 const MONGO_PATH_TICKER = 'ticker';
 const MONGO_PATH_STATISTICS = 'statistics';
 class TickerStatisticsCustomeClass extends bot_1.BotFrameClass {
@@ -32,6 +33,7 @@ class TickerStatisticsCustomeClass extends bot_1.BotFrameClass {
             const maxSpan = Math.max(...this._params.timeSpan);
             const timestamp = Date.now();
             const minTimestamp = timestamp - maxSpan;
+            const ress = {};
             for (const k of this._params.symbols) {
                 const tks = yield this.mongoDB.find(this.getTickerPath(k), {
                     timeStamp: {
@@ -42,10 +44,25 @@ class TickerStatisticsCustomeClass extends bot_1.BotFrameClass {
                     const tk = tks.data[0];
                     const res = this.updateSingleStatics((yield tks).data, this._params.timeSpan);
                     res.pair = tk.pair;
+                    ress[res.pair] = res;
+                    res.ask = (0, utils_general_1.floor)(res.average[0], 2);
+                    res.bid = (0, utils_general_1.floor)(res.average[0], 2);
+                    for (const i in res.average) {
+                        res.average[i] = (0, utils_general_1.floor)(res.average[i], 2);
+                    }
+                    for (const i in res.correlation) {
+                        res.correlation[i] = (0, utils_general_1.floor)(res.correlation[i], 2);
+                    }
+                    for (const i in res.stdv) {
+                        res.stdv[i] = (0, utils_general_1.floor)(res.stdv[i], 2);
+                    }
+                    for (const i in res.slope) {
+                        res.slope[i] = (0, utils_general_1.floor)(res.slope[i], 2);
+                    }
                     yield this.saveToMongoDB(MONGO_PATH_STATISTICS, res, { pair: tk.pair });
-                    yield this.saveToRealtimeDB(__1.MONGO_PATH_BOTSTATISTICS, res);
                 }
             }
+            yield this.saveToRealtimeDB(__1.MONGO_PATH_BOTSTATISTICS, ress);
         });
     }
     getTickerPath(key) {
--- src/lib/statistics/bot.ts
+++ src/lib/statistics/bot.ts
@@ -6,6 +6,7 @@ import {
 } from "../base/bot"
 import { Ticker } from "utils-trade"
 import { MONGO_PATH_BOTSTATISTICS } from "../.."
+import { floor } from "utils-general"
 
 const MONGO_PATH_TICKER = 'ticker'
 const MONGO_PATH_STATISTICS = 'statistics'
@@ -25,6 +26,7 @@ export  abstract class TickerStatisticsCustomeClass extends BotFrameClass {
         const maxSpan = Math.max(...this._params.timeSpan)
         const timestamp = Date.now()
         const minTimestamp = timestamp - maxSpan
+        const ress:{[pair: string]: any} = {}
         for (const k of this._params.symbols) {
             const tks = await this.mongoDB.find(
                 this.getTickerPath(k), {
@@ -37,10 +39,25 @@ export  abstract class TickerStatisticsCustomeClass extends BotFrameClass {
                 const tk = tks.data[0] as Ticker
                 const res = this.updateSingleStatics((await tks).data as Ticker[], this._params.timeSpan)
                 res.pair = tk.pair
+                ress[res.pair] = res
+                res.ask = floor(res.average[0], 2)
+                res.bid = floor(res.average[0], 2)
+                for (const i in res.average) {
+                    res.average[i] = floor(res.average[i], 2)
+                }
+                for (const i in res.correlation) {
+                    res.correlation[i] = floor(res.correlation[i], 2)
+                }
+                for (const i in res.stdv) {
+                    res.stdv[i] = floor(res.stdv[i], 2)
+                }
+                for (const i in res.slope) {
+                    res.slope[i] = floor(res.slope[i], 2)
+                }
                 await this.saveToMongoDB(MONGO_PATH_STATISTICS, res, {pair: tk.pair})
-                await this.saveToRealtimeDB(MONGO_PATH_BOTSTATISTICS, res)
             }
-        }  
+        }
+        await this.saveToRealtimeDB(MONGO_PATH_BOTSTATISTICS, ress)
     }
 
     protected getTickerPath(key: string): string {
